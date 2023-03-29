32ff2cf831f5a10d31fd370da2e99e0f6e36f1f1
Sergei Akimov <sergei_akimov@epam.com>
2023-03-29 21:06:52 +0400

    Test

---
+++ src/test/java/JUnitExamplesTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class JUnitExamplesTest {
+
+    @Test
+    void firstTest() {
+        System.out.println("Here is firstTest()");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Here is secondTest()");
+    }
+}
