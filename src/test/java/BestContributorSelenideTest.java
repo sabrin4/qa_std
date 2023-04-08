765350058cef51d59e51b7b9404351e3a98b0d81
Sergei Akimov <sergei_akimov@epam.com>
2023-04-08 14:30:07 +0400

    added BestContributorSelenideTest

---
+++ src/test/java/BestContributorSelenideTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BestContributorSelenideTest {
+
+
+
+    @Test
+    void BestContributorSelenide() {
+        //открываем страницу, находим нужный элемент
+        open("https://github.com/selenide/selenide");
+        $("div.Layout-sidebar")
+                .$(byText("Contributors"))
+                .closest(".BorderGrid-cell")
+                .$$("ul li").first().hover();
+        // находим имя во всплывающем окне
+        $(".Popover-message").shouldHave(text("Andrei Solntsev"));
+
+
+
+
+
+        sleep(5000);
+    }
+}
+
