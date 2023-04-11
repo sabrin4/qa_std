22afc6aa21a7814373b2a4ce9c159febac5b9a66
Sergei Akimov <sergei_akimov@epam.com>
2023-04-11 19:40:45 +0400

    some temp changes

--- src/test/java/BestContributorSelenideTest.java
+++ src/test/java/BestContributorSelenideTest.java
@@ -1,8 +1,6 @@
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
